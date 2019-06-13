9829082a9686b09443420ca8b13d319e141fbb6d
Tijl Grootswagers <t.grootswagers@gmail.com>
2019-06-13 15:04:27 +1000

    regorganise

--- plot_mean_rdm.m
+++ plot_mean_rdm.m
@@ -23,3 +23,13 @@ print(gcf,'-dpng','-r500',fn)
 im=imread([fn '.png']);
 [i,j]=find(mean(im,3)<255);margin=2;
 imwrite(imcrop(im,[min([j i])-margin range([j i])+2*margin]),[fn '.png'],'png');
+
+%%
+f = figure(1);
+clf;
+f.Position = [f.Position(1:2) 800 600];
+f.PaperPositionMode = 'auto';
+f.PaperOrientation = 'portrait';
+f.Resize = 'on';
+
+imagesc(res.samples)
